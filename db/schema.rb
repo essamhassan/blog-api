bde5c5086be8ab489c9e41ec0f647d98965cbe1e
Essam A. Hassan <es.hassan187@gmail.com>
2016-09-22 18:52:42 +0200

    added avatar to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922164357) do
+ActiveRecord::Schema.define(version: 20160922165040) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.string   "email",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "avatar",     limit: 255
   end
 
 end
